002b84a49cceb442792ad4adde1ec65f1edb2932
Luan Lima <luanemerson8@gmail.com>
2026-05-21 11:02:47 -0300

    Study

--- 7_exemplo/main.go
+++ 7_exemplo/main.go
@@ -6,8 +6,19 @@ func main() {
 
 	var printValue string = "Hello World"
 	printMe(printValue)
+
+	var numerador int = 11
+	var denominador int = 2
+	var result, remainder int = intDivision(numerador, denominador)
+	fmt.Println(result, remainder)
 }
 
 func printMe(printValue string) {
 	fmt.Println(printValue)
+}
+
+func intDivision(numerador int, denominador int) (int, int) {
+	var result int = numerador / denominador
+	var remainder int = numerador % denominador
+	return result, remainder
 }
